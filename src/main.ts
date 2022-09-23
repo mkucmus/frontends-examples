1f90fc110d97912d47926c70e62d7d7856080cef
Docs Keeper <gitlab@shopware.com>
2022-09-23 13:17:55 +0000

    examples update

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import ShopwareFrontends from "./plugins/shopware-frontends";
+import { createRouter, createWebHistory } from "vue-router";
+
+const app = createApp(App);
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/:pathMatch(.*)",
+      component: () => import("./pages/Resolver.vue"),
+    },
+  ],
+});
+app.use(ShopwareFrontends, {
+  shopwareEndpoint: "https://demo-frontends.swstage.store",
+  shopwareAccessToken: "SWSCBHFSNTVMAWNZDNFKSHLAYW",
+  apiDefaults: {},
+});
+
+app.use(router);
+app.mount("#app");
